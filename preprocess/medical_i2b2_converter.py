101fbaea33778712c3c16fa5c82c9a8e49331943
cyy <cyyever@outlook.com>
2024-12-08 17:48:50 +0800

    Fix code

---
+++ preprocess/medical_i2b2_converter.py
@@ -0,0 +1,15 @@
+import argparse
+import json
+from parser import parse_dir
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="Convert Data",
+    )
+    parser.add_argument("--data_dir", help="raw data dir", type=str, required=True)
+    parser.add_argument(
+        "--output_file", help="output json file", type=str, required=True
+    )
+    args = parser.parse_args()
+    parse_dir(args.data_dir)
+    print(args.data_dir)
