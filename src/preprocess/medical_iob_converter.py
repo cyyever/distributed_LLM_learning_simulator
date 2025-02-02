d0837295471543d1c460c0edc019940452841586
cyy <cyyever@outlook.com>
2025-02-02 10:21:21 +0800

    Fix code

--- src/preprocess/medical_iob_converter.py
+++ src/preprocess/medical_iob_converter.py
@@ -13,4 +13,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
     result = parse_dir(args.data_dir, "bio")
     result |= parse_dir(args.data_dir, "iob")
-    print(result)
+    with open(args.output_file, encoding="utf8") as f:
+        json.dump(result, f)
