4279c9c58304b057a0cd812c0773b5a14f7fa819
cyy <cyyever@outlook.com>
2025-02-07 12:25:46 +0800

    Fix code

--- src/method/__init__.py
+++ src/method/__init__.py
@@ -4,8 +4,6 @@ import os
 for entry in os.scandir(os.path.dirname(os.path.abspath(__file__))):
     if not entry.is_dir():
         continue
-    if entry.name == "__pycache__":
-        continue
-    if entry.name.startswith("."):
+    if entry.name.startswith(".") or entry.name.startswith("__"):
         continue
     importlib.import_module(f".{entry.name}", "method")
--- src/method/adaptor_avg/server.py
+++ src/method/adaptor_avg/server.py
@@ -1,3 +1,4 @@
+from typing import Any
 from cyy_torch_toolbox import Inferencer, TensorDict, TextDatasetCollection
 
 from ..method_forward import LLMTextServer
@@ -5,8 +6,8 @@ from .data_pipeline import get_pipeline
 
 
 class FinetuneAdaptorServer(LLMTextServer):
-    def get_tester(self, *args, **kwargs) -> Inferencer:
-        inferencer = super().get_tester(**kwargs)
+    def get_tester(self, *args: Any, **kwargs: Any) -> Inferencer:
+        inferencer = super().get_tester(*args, **kwargs)
         assert isinstance(inferencer.dataset_collection, TextDatasetCollection)
         for transform in get_pipeline().transforms:
             inferencer.dataset_collection.append_text_transform(transform)
