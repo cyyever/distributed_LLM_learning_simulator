a27e280443c128f6729766abb1e834b35d59b10d
cyy <cyyever@outlook.com>
2025-02-09 14:46:31 +0800

    Add code

--- src/method/adaptor_avg/worker.py
+++ src/method/adaptor_avg/worker.py
@@ -1,3 +1,4 @@
+from cyy_naive_lib.log import log_info
 from ..method_forward import FinetuneAdaptorWorker, get_iob_pipeline
 
 
@@ -6,3 +7,5 @@ class NERWorker(FinetuneAdaptorWorker):
         super()._before_training()
         for transform in get_iob_pipeline().transforms:
             self.dataset_collection.append_text_transform(transform)
+        if self.hold_log_lock:
+            log_info("model is %s", self.trainer.model)
