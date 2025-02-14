3fcec581439f73e02f5652a64b74e063a1706729
cyy <cyyever@outlook.com>
2025-02-14 18:37:32 +0800

    Fix code

--- src/datapipeline_mixin.py
+++ src/datapipeline_mixin.py
@@ -7,7 +7,7 @@ class DatapipelineMixin(ExecutorProtocol):
     def read_prompt(self) -> str:
         prompt_file = self.config.dc_config.dataset_kwargs["prompt_file"]
         prompt_file = os.path.join(
-            os.path.dirname(os.path.realpath(__file__)), "prompt", prompt_file
+            os.path.dirname(os.path.realpath(__file__)), "..", "prompt", prompt_file
         )
         with open(prompt_file, encoding="utf8") as f:
             return f.read()
