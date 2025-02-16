b84c5d0fc4fd82af728590aa59103654ec91b15a
cyy <cyyever@outlook.com>
2025-02-16 17:17:41 +0800

    Fix code

--- simulator.py
+++ simulator.py
@@ -1,8 +1,8 @@
 import os
-
-os.environ["WANDB_DISABLED"] = "true"
 import sys
 
+os.environ["WANDB_DISABLED"] = "true"
+from cyy_naive_lib.log import log_warning
 from distributed_learning_simulation import (
     load_config,
     train,
@@ -18,4 +18,6 @@ if __name__ == "__main__":
         config_path=config_path,
         global_conf_path=os.path.join(config_path, "global.yaml"),
     )
+    if config.use_amp:
+        log_warning("AMP may slowdown training and increase GPU memory")
     train(config=config, single_task=True)
--- test.sh
+++
@@ -1,2 +0,0 @@
-python3 ./simulator.py --config-name medical_ner/Meta-Llama-3-1B-Instruct.yaml ++medical_ner.round=1 ++medical_ner.epoch=5 ++medical_ner.worker_number=10
-# python3 ./simulator.py --config-name medical_ner/DeepSeek-R1-Distill-Llama-8B.yaml ++medical_ner.round=1 ++medical_ner.epoch=1 ++medical_ner.worker_number=10
