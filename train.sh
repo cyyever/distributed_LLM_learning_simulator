85bdb9c686651380693b9fa4b5b7dba31b4f7fae
cyy <cyyever@outlook.com>
2025-02-19 10:31:23 +0800

    Add sh

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python3 ./simulator.py --config-name medical_ner/Meta-Llama-3-1B-Instruct.yaml ++medical_ner.round=1 ++medical_ner.epoch=2 ++medical_ner.worker_number=3
+python3 ./simulator.py --config-name medical_ner/Meta-Llama-3-7B.yaml  ++medical_ner.round=3 ++medical_ner.epoch=2 ++medical_ner.worker_number=3
