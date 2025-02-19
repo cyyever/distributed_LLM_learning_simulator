9a9cc297c59fed00f731efc3cde6861d73300530
cyy <cyyever@outlook.com>
2025-02-19 10:35:41 +0800

    Add sh

--- convert_test.sh
+++
@@ -1,6 +0,0 @@
-rm -rf /home/cyy/NER_data/test
-mkdir -p /home/cyy/NER_data/test
-for dir in /home/cyy/NER_bio/test/*; do
-  python3 src/command/medical_iob_converter.py --data_dir=${dir} --output_file=$(basename ${dir}).json
-  mv $(basename ${dir}).json /home/cyy/NER_data/test/
-done
