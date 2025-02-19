7d84f5e21047950e2a9f7e3725d6d7c02b00ff10
cyy <cyyever@outlook.com>
2025-02-19 21:04:08 +0800

    Fix code

--- src/sft/__init__.py
+++ src/sft/__init__.py
@@ -44,6 +44,7 @@ def get_SFTConfig(config: Config, executor: Executor, output_dir: str) -> SFTCon
         gradient_checkpointing=False,
         report_to="none",
         warmup_ratio=0.05,
+        logging_nan_inf_filter=False,
         max_seq_length=config.dc_config.dataset_kwargs.get("input_max_len", 1024),
     )
 
