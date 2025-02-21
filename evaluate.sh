dec9116fe26329c732de0d84ab7d6df70fb62514
cyy <cyyever@outlook.com>
2025-02-21 22:37:09 +0800

    Fix code

--- evaluate.sh
+++ evaluate.sh
@@ -1,2 +1,2 @@
-export SESSION_DIR=session/adaptor_avg/hugging_face_yale_ner_random_split/hugging_face_causal_lm_meta-llama/Llama-3.2-1B-Instruct/2025-02-17_14_23_04/323085876743497509031697659185328997202
-python3 ./src/command/medical_NER_evaluator.py --config-name medical_ner/Meta-Llama-3-1B-Instruct.yaml
+export SESSION_DIR="session/adaptor_avg/hugging_face_yale_ner_random_split/hugging_face_causal_lm_deepseek-ai/DeepSeek-R1-Distill-Llama-8B/2025-02-21_15_15_14/167313486424081317415176708380560719238"
+python3 ./src/command/medical_NER_evaluator.py
