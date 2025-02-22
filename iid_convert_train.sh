217b887a3fed4ce81cf5634e70771efbdce38597
cyy <cyyever@outlook.com>
2025-02-22 16:09:22 +0800

    Fix code

--- iid_convert_train.sh
+++ iid_convert_train.sh
@@ -1,5 +1,6 @@
 NER_bio_dir=/home/cyy/NER_bio/train
 NER_json_dir=/home/cyy/NER_data_train_iid
-iid_number=4
+sub_dirs="MIMIC3:UTP:MTSamples"
+iid_number=2
 
-python3 src/command/medical_iob_distribution.py --data_dir="${NER_bio_dir}" --sub_dirs='MIMIC3:UTP:MTSamples' --skip_empty=1 --split_number=${iid_number} --output_dir="${NER_json_dir}"
+python3 src/command/medical_iob_distribution.py --data_dir="${NER_bio_dir}" --sub_dirs="${sub_dirs}" --skip_empty=1 --split_number=${iid_number} --output_dir="${NER_json_dir}"
