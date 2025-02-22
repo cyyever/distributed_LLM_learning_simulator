6cae58220c3d4f557f7c5a4dfb35146fbd0d636a
cyy <cyyever@outlook.com>
2025-02-22 13:07:44 +0800

    Fix code

--- src/command/medical_iob_distribution.py
+++ src/command/medical_iob_distribution.py
@@ -1,15 +1,15 @@
 import argparse
-import json
-from dataclasses import dataclass, field
 import collections
+import json
 import os
 import sys
+from dataclasses import dataclass, field
 
 lib_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..")
 sys.path.append(lib_path)
-from preprocess import parse_dir
-from preprocess import IOBRecord
 import heapq
+
+from preprocess import IOBRecord, parse_dir
 
 
 @dataclass(order=True)
@@ -26,7 +26,7 @@ class RecordCount:
 
 def get_min_tag(all_records: list[IOBRecord]) -> str | None:
     assert all_records
-    total_counter = collections.Counter()
+    total_counter: collections.Counter[str] = collections.Counter()
     for r in all_records:
         assert isinstance(r, IOBRecord)
         counter = collections.Counter(
@@ -82,6 +82,7 @@ if __name__ == "__main__":
         prog="Analyze IBO Distribution",
     )
     parser.add_argument("--data_dir", help="raw data dir", type=str, required=True)
+    parser.add_argument("--sub_dirs", help="raw data sub-dir", type=str, default=None)
     parser.add_argument(
         "--skip_empty", help="skip empty lines", type=bool, default=True
     )
@@ -89,25 +90,35 @@ if __name__ == "__main__":
         "--split_number", help="number to split", type=int, required=True
     )
     parser.add_argument(
-        "--output_dir", help="output dir for json files", type=str, required=True
+        "--output_dir",
+        help="output dir for json files",
+        type=str,
+        required=True,
     )
     args = parser.parse_args()
     os.environ["SKIP_EMPTY_LINE"] = str(int(args.skip_empty))
     split_number = int(args.split_number)
+    allocation: dict[int, list] = {i: [] for i in range(split_number)}
+    data_dir = args.data_dir
+    if args.sub_dirs is not None:
+        data_dirs = [os.path.join(data_dir, s) for s in args.sub_dirs.split(":")]
+    else:
+        data_dirs = [data_dir]
 
-    result = parse_dir(args.data_dir, "bio")
-    result |= parse_dir(args.data_dir, "iob")
-    all_records = []
-    for records in result.values():
-        all_records += records
-    assert all_records
-    allocation = {i: [] for i in range(split_number)}
-    allocate(all_records=all_records, allocation=allocation, split_number=split_number)
-    allocated_cnt = 0
+    for data_dir in data_dirs:
+        assert os.path.isdir(data_dir)
+        print("check ",data_dir)
+        result = parse_dir(data_dir, "bio")
+        result |= parse_dir(args.data_dir, "iob")
+        all_records = []
+        for records in result.values():
+            all_records += records
+        assert all_records
+        allocate(
+            all_records=all_records, allocation=allocation, split_number=split_number
+        )
     for records in allocation.values():
-        total_counter = collections.Counter()
-        allocated_cnt += len(records)
-
+        total_counter: collections.Counter[str] = collections.Counter()
         for r in records:
             assert isinstance(r, IOBRecord)
             counter = collections.Counter(
