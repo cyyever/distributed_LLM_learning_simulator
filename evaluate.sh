93d1bc1a1a2580fd74df2ad7490034b70ee29f17
cyy <cyyever@outlook.com>
2025-02-23 12:06:57 +0800

    Fix code

--- evaluate.sh
+++ evaluate.sh
@@ -1,2 +1,6 @@
-export SESSION_DIR="session/adaptor_avg/hugging_face_yale_ner_random_split/hugging_face_causal_lm_deepseek-ai/DeepSeek-R1-Distill-Llama-8B/2025-02-21_15_15_14/167313486424081317415176708380560719238"
-python3 ./src/command/medical_NER_evaluator.py
+export SESSION_DIR="session/adaptor_avg/hugging_face_yale_ner_iid/hugging_face_causal_lm_deepseek-ai/DeepSeek-R1-Distill-Llama-8B/2025-02-22_22_09_46/240613044987893863958513903783342351560"
+test_dir=/home/cyy/NER_data/test/
+# for file in MIMIC3.json UTP.json MTSamples.json; do
+for file in MTSamples.json; do
+  python3 ./src/command/medical_NER_evaluator.py --session_dir="${SESSION_DIR}" --test_file="${test_dir}/${file}"
+done
