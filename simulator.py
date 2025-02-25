998c7162b5749656ddf29b7e3cb55993caf5509e
cyy <cyyever@outlook.com>
2025-02-25 12:27:52 +0800

    Fix code

--- simulator.py
+++ simulator.py
@@ -1,10 +1,9 @@
-import logging
 import os
 import sys
 from contextlib import redirect_stdout
 
 from cyy_naive_lib.log import (
-    initialize_proxy_logger,
+    StreamToLogger,
     replace_default_logger,
     replace_logger,
 )
@@ -15,23 +14,6 @@ from distributed_learning_simulation import (
     load_config,
     train,
 )
-
-
-class StreamToLogger:
-    """
-    Fake file-like stream object that redirects writes to a logger instance.
-    """
-
-    def __init__(self, logger) -> None:
-        self.logger = logger
-
-    def write(self, buf) -> None:
-        for line in buf.rstrip().splitlines():
-            self.logger.info(line.rstrip())
-
-    def flush(self) -> None:
-        pass
-
 
 config_path = os.path.join(os.path.dirname(__file__), "conf")
 src_path = os.path.join(config_path, "..", "src")
@@ -49,15 +31,14 @@ if __name__ == "__main__":
         sys.argv.append(option)
     replace_default_logger()
     replace_logger("transformers")
-    initialize_proxy_logger()
-    config = load_config(
-        config_path=config_path,
-        global_conf_path=os.path.join(config_path, "global.yaml"),
-    )
-    config.heavy_server = False
-    if config.trainer_config.hook_config.use_amp:
-        log_warning("AMP may slowdown training and increase GPU memory")
-    config.preallocate_device = True
 
-    with redirect_stdout(StreamToLogger(logging.getLogger())):
+    with redirect_stdout(StreamToLogger()):
+        config = load_config(
+            config_path=config_path,
+            global_conf_path=os.path.join(config_path, "global.yaml"),
+        )
+        config.heavy_server = False
+        if config.trainer_config.hook_config.use_amp:
+            log_warning("AMP may slowdown training and increase GPU memory")
+        config.preallocate_device = True
         train(config=config, single_task=True)
