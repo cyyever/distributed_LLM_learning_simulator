38760521a96dbed9a71a357358bf352dfc636599
cyy <cyyever@outlook.com>
2025-02-28 14:27:03 +0800

    Fix script

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python3 ./simulator.py --config-name medical_ner/Meta-Llama-3-7B.yaml  ++medical_ner.round=3 ++medical_ner.epoch=2 ++medical_ner.worker_number=3
+python3 ./simulator.py --config-name medical_ner/Meta-Llama-3-8B.yaml  ++medical_ner.round=3 ++medical_ner.epoch=2 ++medical_ner.worker_number=3
