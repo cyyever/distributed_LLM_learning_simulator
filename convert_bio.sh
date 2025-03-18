306ba7a2b4314f4c633611b76934c868e047d49e
cyy <cyyever@outlook.com>
2025-03-18 17:44:57 +0800

    Fix code

--- convert_bio.sh
+++ convert_bio.sh
@@ -1,11 +1,10 @@
-NER_bio_dir=/home/cyy/NER_bio
-NER_json_dir=/home/cyy/NER_data
+NER_bio_dir=$HOME/NER_bio
+NER_json_dir=$HOME/NER_data
 mkdir -p ${NER_json_dir}/train
 for dir in ${NER_bio_dir}/train/*; do
   python3 src/command/medical_iob_converter.py --data_dir=${dir} --output_file=$(basename ${dir}).json
   mv $(basename ${dir}).json ${NER_json_dir}/train/
 done
-
 
 mkdir -p ${NER_json_dir}/test
 for dir in ${NER_bio_dir}/test/*; do
--- evaluate.sh
+++ evaluate.sh
@@ -1,5 +1,5 @@
 export SESSION_DIR="session/adaptor_avg/hugging_face_yale_ner_iid/hugging_face_causal_lm_deepseek-ai/DeepSeek-R1-Distill-Llama-8B/2025-02-22_22_09_46/240613044987893863958513903783342351560"
-test_dir=/home/cyy/NER_data/test/
+test_dir=$HOME/NER_data/test/
 # for file in MIMIC3.json UTP.json MTSamples.json; do
 for file in MTSamples.json; do
   python3 ./src/command/medical_NER_evaluator.py --session_dir="${SESSION_DIR}" --test_file="${test_dir}/${file}"
