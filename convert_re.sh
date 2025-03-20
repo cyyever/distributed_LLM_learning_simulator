46f92943dd42fbcb7ed02d82c930c5ddeb2d2c38
cyy <cyyever@outlook.com>
2025-03-20 12:29:52 +0800

    Fix code

--- convert_re.sh
+++ convert_re.sh
@@ -2,8 +2,8 @@ RE_csv_dir=${HOME}/RE/RE
 RE_json_dir=${HOME}/RE_data
 
 mkdir -p ${RE_json_dir}
-python3 src/command/csv_parse.py --csv_files "${RE_csv_dir}/RE_train.csv ${RE_csv_dir}/RE_dev.csv" --output_file="${RE_json_dir}/train.json"
+python3 src/command/medical_re_csv_parse.py --csv_files "${RE_csv_dir}/RE_train.csv ${RE_csv_dir}/RE_dev.csv" --output_file="${RE_json_dir}/train.json"
 
 for csvfile in RE_i2b2_test RE_MIMIC3_test RE_MTSample_test RE_UTP_test; do
-  python3 src/command/csv_parse.py --csv_files "${RE_csv_dir}/${csvfile}.csv" --output_file="${RE_json_dir}/${csvfile}.json"
+  python3 src/command/medical_re_csv_parse.py --csv_files "${RE_csv_dir}/${csvfile}.csv" --output_file="${RE_json_dir}/${csvfile}.json"
 done
