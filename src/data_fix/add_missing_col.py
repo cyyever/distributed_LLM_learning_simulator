15d10fe3bf6c677ecabf86024f5445976ef7f508
cyy <cyyever@outlook.com>
2025-03-28 20:53:58 +0800

    Fix code

---
+++ src/data_fix/add_missing_col.py
@@ -0,0 +1,23 @@
+import argparse
+import os
+import json
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="Add missing column to json files",
+    )
+    parser.add_argument("--file", help="raw data file", type=str, required=True)
+    args = parser.parse_args()
+    assert os.path.isfile(args.file)
+    res = None
+    print(args.file)
+    with open(args.file, "rt", encoding="utf8") as f:
+        res = json.load(f)
+        assert isinstance(res, list)
+        for a in res:
+            assert isinstance(a, dict)
+            for k in ("input", "output", "html"):
+                if k not in a:
+                    a[k] = ""
+    with open(args.file, "wt", encoding="utf8") as f:
+        json.dump(res, f)
