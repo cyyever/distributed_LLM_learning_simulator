6efb9e26f63a1e3f83623e3359a51a12313bc245
cyy <cyyever@outlook.com>
2025-04-28 22:29:46 +0800

    Fix code

--- src/method/adaptor_avg/__init__.py
+++ src/method/adaptor_avg/__init__.py
@@ -11,12 +11,6 @@ from ..method_forward import (
 )
 
 AlgorithmRepository.register_algorithm(
-    algorithm_name="adaptor_avg_old",
-    client_cls=FinetuneAdaptorWorker,
-    server_cls=FinetuneAdaptorServer,
-    algorithm_cls=FedAVGAlgorithm,
-)
-AlgorithmRepository.register_algorithm(
     algorithm_name="adaptor_avg",
     client_cls=SFTTrainerWorker,
     server_cls=SFTServer,
