b7cf3d35cd77070f7c5f77a7a1d37b6f5ee5c18e
cyy <cyyever@outlook.com>
2025-04-28 23:00:24 +0800

    Fix code

--- src/method/FedSA_LoRA/worker.py
+++ src/method/FedSA_LoRA/worker.py
@@ -11,3 +11,10 @@ class FedSALoRAWorker(SFTTrainerWorker):
         state = {k: v for k, v in state.items() if "lora_A" in k}
         assert state
         return state
+
+    def _load_adaptor(self, adaptor_parameter: TensorDict) -> None:
+        state = super()._get_parameters()
+        for k, v in adaptor_parameter.items():
+            assert k in state
+            state[k] = v
+        super()._load_adaptor(state)
