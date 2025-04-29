08c2e11e1eaf898e49713598dd7ccfe955cdee3c
cyy <cyyever@outlook.com>
2025-04-29 23:20:18 +0800

    Fix code

--- src/method/FedSA_LoRA/worker.py
+++ src/method/FedSA_LoRA/worker.py
@@ -13,19 +13,18 @@ class FedSALoRAWorker(SFTTrainerWorker):
         self.old_state = tensor_clone(super()._get_parameters())
         assert self.old_state is not None
         state = {k: v for k, v in self.old_state.items() if "lora_A" in k}
-        log_info("Upload layers:%s", list(state.keys()))
         assert state
         return state
 
     def _load_adaptor(self, adaptor_parameter: TensorDict) -> None:
-        if self._sft_trainer is None:
+        if self.old_state is None:
             super()._load_adaptor(adaptor_parameter)
             return
 
-        assert self.old_state is not None
         adaptor_parameter = {
             k: v for k, v in adaptor_parameter.items() if "lora_A" in k
         }
+        log_info("Received layers:%s", list(adaptor_parameter.keys()))
         assert adaptor_parameter
         for k, v in adaptor_parameter.items():
             assert k in self.old_state
