173423d768d1e8d722e0cabd375567d80c4d2193
cyy <cyyever@outlook.com>
2025-04-29 09:59:56 +0800

    Add logging

--- simulator.py
+++ simulator.py
@@ -1,10 +1,11 @@
 import os
 import sys
 
+
 os.environ["TQDM_DISABLE"] = "1"
 os.environ["LEAST_REQUIRED_DEVICE_MEMORY_IN_GB"] = "10"
 
-from cyy_naive_lib.log import redirect_stdout_to_logger
+from cyy_naive_lib.log import redirect_stdout_to_logger, log_info
 from distributed_learning_simulation import (
     load_config,
     train,
@@ -18,4 +19,6 @@ if __name__ == "__main__":
     config_path = os.path.join(project_path, "conf")
     with redirect_stdout_to_logger(logger_names=["transformers"]):
         config = load_config(config_path=config_path, import_libs=False)
+        log_info("Use train files %s", config.dc_config.dataset_kwargs["train_files"])
+        log_info("Use test files %s", config.dc_config.dataset_kwargs["test_files"])
         train(config=config, single_task=True)
