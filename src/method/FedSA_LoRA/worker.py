f445085b1ef0afa422650f798d8b56bd0597c6b3
cyy <cyyever@outlook.com>
2025-04-29 10:34:01 +0800

    Fix code

--- src/method/FedSA_LoRA/worker.py
+++ src/method/FedSA_LoRA/worker.py
@@ -1,5 +1,6 @@
-from cyy_torch_toolbox import TensorDict
+
 from cyy_naive_lib.log import log_info
+from cyy_torch_toolbox import TensorDict, tensor_clone
 
 from ..method_forward import (
     SFTTrainerWorker,
@@ -7,16 +8,22 @@ from ..method_forward import (
 
 
 class FedSALoRAWorker(SFTTrainerWorker):
+    old_state: None | TensorDict = None
+
     def _get_parameters(self) -> TensorDict:
-        state = super()._get_parameters()
-        state = {k: v for k, v in state.items() if "lora_A" in k}
+        self.old_state = tensor_clone(super()._get_parameters())
+        assert self.old_state is not None
+        state = {k: v for k, v in self.old_state.items() if "lora_A" in k}
         log_info("Upload layers:%s", list(state.keys()))
         assert state
         return state
 
     def _load_adaptor(self, adaptor_parameter: TensorDict) -> None:
-        state = super()._get_parameters()
+        if self.old_state is None:
+            super()._load_adaptor(adaptor_parameter)
+            return
+
         for k, v in adaptor_parameter.items():
-            assert k in state
-            state[k] = v
-        super()._load_adaptor(state)
+            assert k in self.old_state
+            self.old_state[k] = v
+        super()._load_adaptor(self.old_state)
