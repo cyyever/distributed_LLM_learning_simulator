a4c4af26aa94bab584ae20654010d99f9454fbde
cyy <cyyever@outlook.com>
2025-04-30 10:01:56 +0800

    Fix code

---
+++ src/method/FedSA_LoRA/server.py
@@ -0,0 +1,13 @@
+from cyy_naive_lib.log import log_warning
+from ..method_forward import (
+    SFTServer,
+)
+
+
+class FedSALoRAServer(SFTServer):
+    def _stopped(self) -> bool:
+        return self._stop
+
+    def _server_exit(self) -> None:
+        log_warning("Store final session")
+        super()._server_exit()
