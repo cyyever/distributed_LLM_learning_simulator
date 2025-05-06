1049def1aff1482a0fc95c658a764b7df5fa33ac
cyy <cyyever@outlook.com>
2025-05-06 14:07:57 +0800

    Fix code

--- src/method/adaptor_avg/__init__.py
+++ src/method/adaptor_avg/__init__.py
@@ -3,14 +3,17 @@ from distributed_learning_simulation import (
     FedAVGAlgorithm,
 )
 
-from ..method_forward import (
-    SFTServer,
-    SFTTrainerWorker,
-)
+from ..method_forward import AggregationByLossAlgorithm, SFTServer, SFTTrainerWorker
 
 AlgorithmRepository.register_algorithm(
     algorithm_name="adaptor_avg",
     client_cls=SFTTrainerWorker,
     server_cls=SFTServer,
     algorithm_cls=FedAVGAlgorithm,
+)
+AlgorithmRepository.register_algorithm(
+    algorithm_name="adaptor_avg_by_loss",
+    client_cls=SFTTrainerWorker,
+    server_cls=SFTServer,
+    algorithm_cls=AggregationByLossAlgorithm,
 )
--- src/method/method_forward.py
+++ src/method/method_forward.py
@@ -4,6 +4,7 @@
 # lib_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..")
 # sys.path.append(lib_path)
 
+from ..algorithm import *
 from ..datapipeline_mixin import *
 from ..preprocess import *
 from ..server import *
