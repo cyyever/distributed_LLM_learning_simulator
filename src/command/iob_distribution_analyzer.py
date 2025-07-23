5bbe6012e590ddf5bff4724995aa16d83d8f4442
cyy <cyyever@outlook.com>
2025-07-23 13:00:20 +0800

    Fix code

--- src/command/iob_distribution_analyzer.py
+++ src/command/iob_distribution_analyzer.py
@@ -5,29 +5,35 @@ import sys
 lib_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..")
 sys.path.append(lib_path)
 
-from preprocess import parse_dir
-from preprocess.distribution import token_distribution
+from preprocess import parse_dir, parse_file
+from preprocess.distribution import token_distribution, tag_distribution
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="Analyze IBO Distribution",
     )
-    parser.add_argument("--data_dir", help="raw data dir", type=str, required=True)
+    parser.add_argument("--data_dir", help="raw data dir", type=str, default=None)
     parser.add_argument("--sub_dirs", help="raw data sub-dir", type=str, default=None)
+    parser.add_argument("--file", help="raw data dir", type=str, default=None)
     args = parser.parse_args()
-    data_dir = args.data_dir
-    if args.sub_dirs is not None:
-        data_dirs = [os.path.join(data_dir, s) for s in args.sub_dirs.split(":")]
+    all_records = []
+    if args.file is not None:
+        print("check ", args.file)
+        all_records += parse_file(args.file)
     else:
-        data_dirs = [data_dir]
-
-    for data_dir in data_dirs:
-        assert os.path.isdir(data_dir)
-        print("check ", data_dir)
-        result = parse_dir(data_dir, "bio")
-        result |= parse_dir(args.data_dir, "iob")
-        all_records = []
+        data_dir = args.data_dir
+        if args.sub_dirs is not None:
+            data_dirs = [os.path.join(data_dir, s) for s in args.sub_dirs.split(":")]
+        else:
+            data_dirs = [data_dir]
+        result: dict = {}
+        for data_dir in data_dirs:
+            assert os.path.isdir(data_dir)
+            print("check ", data_dir)
+            result |= parse_dir(data_dir, "bio")
+            result |= parse_dir(args.data_dir, "iob")
         for records in result.values():
             all_records += records
-        assert all_records
-        token_distribution(all_records=all_records)
+    assert all_records
+    print("tag counts", tag_distribution( all_records))
+    # token_distribution(all_records=all_records)
