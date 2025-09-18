896fa798e779a6fd2af8302e41b2cef0a25fb1fe
Yuanyuan Chen <cyyever@outlook.com>
2025-09-18 15:25:27 +0800

    Fix code

--- json_converter.py
+++ json_converter.py
@@ -1,5 +1,5 @@
 import json
-import os
+
 from cyy_naive_lib import load_json
 
 
@@ -22,7 +22,11 @@ def convert_json_to_ner(input_json):
     sentences = []
     sentences_begin = []
     sentences_end = []
-    for token_info in res["indexes"].values():
+    sentence_tokens = []
+    sentence_tags = []
+    last_tag: str | None = None
+    last_tag_end: int | None = None
+    for key, token_info in res["indexes"].items():
         assert isinstance(token_info, dict)
         if "Sentence" in token_info:
             sentence = token_info["Sentence"]
@@ -36,8 +40,41 @@ def convert_json_to_ner(input_json):
             sentences.append(sentence)
             sentences_begin.append(begin)
             sentences_end.append(end)
+            sentence_tokens.append([])
+            sentence_tags.append([])
+        if "Token" in token_info:
+            assert len(token_info["Token"]) == 1
+            token_begin = token_info["Token"][0]["begin"]
+            token_end = token_info["Token"][0]["end"]
+            assert token_begin >= sentences_begin[-1] and token_end <= sentences_end[-1]
+            sentence_tokens[-1].append(content[token_begin:token_end])
+            if last_tag_end is not None and token_end <= last_tag_end + 1:
+                sentence_tags[-1].append(f"I-{last_tag}")
+            else:
+                sentence_tags[-1].append("O")
+        if "Entity" in token_info:
+            entity = token_info["Entity"]
+            assert isinstance(entity, list)
+            # assert len(entity)==1,key
+            for e in entity:
+                begin = e["begin"]
+                end = e["end"]
+                semantic = e["semantic"].lower()
+                if semantic in ("problem", "treatment", "test", "drug"):
+                    assert "Token" in token_info, key
+                    # assert begin == token_begin, key
+                    if begin != token_begin:
+                        print("skip annotation", key)
+                        continue
+                    ner_annotations[begin] = {"ner": (begin, end, semantic)}
+                    last_tag_end = end
+                    last_tag = semantic
+                    sentence_tags[-1][-1] = f"B-{last_tag}"
 
     assert sentences
+    assert len(sentences) == len(sentence_tokens)
+    for t in sentence_tokens:
+        assert t
     sentence_idx = len(sentences) - 1
     last_sentence_begin = -1
     last_sentence_end = -1
@@ -66,13 +103,18 @@ def convert_json_to_ner(input_json):
                 + sentence[end:]
             )
             sentences[sentence_idx] = sentence
-    for line in sentences:
-        print(line)
-        pass
+    # for line in sentences:
+    #     # print(line)
+    #     pass
+    res = []
     with open(
-        input_json.removesuffix(".json") + "_NER.json", "tw", encoding="utf8"
+        input_json.removesuffix(".json") + "_NER.json", "w", encoding="utf8"
     ) as f:
-        json.dump(content, f)
+        for sentence, tags, tokens in zip(
+            sentences, sentence_tags, sentence_tokens, strict=False
+        ):
+            res.append({"tokens": tokens, "tags": tags, "html": sentence})
+        json.dump(res, f)
 
 
 convert_json_to_ner("/home/cyy/10708968_887748326.json")
